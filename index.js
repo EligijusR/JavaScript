160b1044a814acb5aacfecbffb8696ccad7672ec
EligijusR <eligijus.ramanauskas2@gmail.com>
2022-03-10 22:04:58 +0200

    New branch

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// Skaičiai//
+
+let pirmas = 13;
+let antras = 35;
+let liekana = pirmas % antras;
+console.log(liekana)
+
+let vardas = 'Eligijus';
+let pavarde = 'Ramanauskas';
+let gimimoMetai = 1997;
+let dabartiniaiMetai = 2022;
+tekstas= `${vardas} ${pavarde} man yra ${dabartiniaiMetai-gimimoMetai} metai`
+console.log(tekstas.length)
+kitasVardas = 'MANI varDas Yra';
+console.log(kitasVardas.charAT(0));
